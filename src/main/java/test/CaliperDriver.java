9349bc4028fd703dec5aca7706d285296705c530
shawn <Shawn.Chou@nogle.com>
2015-08-04 14:28:36 +0800

    remove useless class

--- src/main/java/test/CaliperDriver.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class CaliperDriver {
-
-	public static void main(String[] args) {
-		com.google.caliper.runner.CaliperMain.main(new String[] {"test.BanchMarkTest.java"});
-	}
-}
