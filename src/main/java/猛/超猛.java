8c1adf2dd023d767f6a9e295dd41697df877b75f
Shawn <qqdog1@yahoo.com.tw>
2020-12-10 18:15:08 +0800

    1. 小時候玩物

---
+++ src/main/java/猛/超猛.java
@@ -0,0 +1,16 @@
+package 猛;
+
+public class 超猛  <猛字串 extends String>{
+	public static void main(String[] 字串) {
+		new 超猛();
+	}
+	
+	public 超猛() {
+		猛字串 猛猛der = (猛字串) "猛猛der";
+		印(猛猛der);
+	}
+	
+	private void 印(猛字串 想印的字) {
+		System.out.println(想印的字);
+	}
+}
